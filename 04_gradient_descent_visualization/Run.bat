ae9d4af5bd4172a6339e8a2ee8fbd56c68c4e75f
Urs Stutz <gitter@ls3d.de>
2016-06-29 15:21:55 +0200

    gradient descent visualization

---
+++ 04_gradient_descent_visualization/Run.bat
@@ -0,0 +1,4 @@
+@echo off
+cd bin
+python Gradientdescent1.py
+pause
---
+++ 04_gradient_descent_visualization/src/Data.hx
@@ -0,0 +1,17 @@
+package;
+
+/**
+ * ...
+ * @author Urs Stutz
+ */
+class Data {
+	
+	public var x:Float;
+	public var y:Float;
+
+	public function new( x:Float, y:Float ) {
+		this.y = y;
+		this.x = x;
+	}
+	
+}
